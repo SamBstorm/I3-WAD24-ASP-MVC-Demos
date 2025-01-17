f3e4fddc66fb56b20655b782c2eb6dafbb318e64
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-17 13:44:18 +0100

    Add Demo : ASP .net MVC - Les contrôleurs et vues

---
+++ Demo-ASP-01/Controllers/DemoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_ASP_01.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+            //Dans le cas où le nom de la vue ne correspond pas au nom de l'action
+            //return View("IndexOld");
+        }
+    }
+}
