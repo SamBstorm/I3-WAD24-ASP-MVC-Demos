8e6e06c75f88198709fe9caf10e50b5cdb525b29
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-22 11:09:40 +0100

    Add Demo : ASP.net MVC - ViewData/ViewBag/TempData

--- Demo-ASP-01/Controllers/DemoController.cs
+++ Demo-ASP-01/Controllers/DemoController.cs
@@ -9,6 +9,8 @@ namespace Demo_ASP_01.Controllers
         public IActionResult Index()
         {
             Title = "Accueil";
+            //// Pour démontrer l'utilisation de variable de type dynamic
+            //DemoDynamic();
             return View();
 
             //Dans le cas où le nom de la vue ne correspond pas au nom de l'action
@@ -23,5 +25,40 @@ namespace Demo_ASP_01.Controllers
             return View();
         }
 
+        [Route("Demo/DynamicTable/{nb}")]
+        public IActionResult DynamicTable(int nb)
+        {
+            Title = $"Table de {nb}";
+            ViewBag.table = nb;
+            return View("Table");
+        }
+
+        [NonAction]
+        public void DemoDynamic()
+        {
+            dynamic varDynamic;
+            varDynamic = "Ceci est une valeur de type string";
+            Console.WriteLine(varDynamic.Substring(0,5));
+            varDynamic = true;
+            if(varDynamic) Console.WriteLine($"C'est vrai!");
+            varDynamic = 3.141529;
+            Console.WriteLine(varDynamic * 30);
+        }
+
+        [Route("Demo/SaveData/{data}")]
+        public IActionResult SaveData(string data)
+        {
+            TempData["data"] = data;
+            return View();
+        }
+
+        public IActionResult ShowData()
+        {
+            if (TempData.ContainsKey("data"))
+            {
+                TempData.Keep("data");
+            }
+            return View();
+        }
     }
 }
