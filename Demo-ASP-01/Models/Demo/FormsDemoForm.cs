f67f843a4609a3bb5a7e5232e59b3c1e2fb9fe22
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-24 11:28:56 +0100

    Update Demo : ASP.net MVC - Formulaires (HtmlHelpers-Part03)

--- Demo-ASP-01/Models/Demo/FormsDemoForm.cs
+++ Demo-ASP-01/Models/Demo/FormsDemoForm.cs
@@ -1,21 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Demo_ASP_01.Models.Demo
 {
     public class FormsDemoForm
     {
+        [DisplayName("Titre : ")]
         [Required(ErrorMessage = "Veuillez choisir un Titre")]
         public string Title { get; set; }
+
+        [DisplayName("Nom de famille : ")]
         [Required(ErrorMessage = "Le champs Nom de famille est obligatoire.")]
         [MinLength(2, ErrorMessage = "Le champs Nom de famille doit contenir au minimum 2 caractères.")]
         [MaxLength(64, ErrorMessage = "Le champs Nom de famille doit contenir au maximum 64 caractères.")]
         public string LastName { get; set; }
 
+        [DisplayName("Prénom : ")]
         [Required(ErrorMessage = "Le champs Prénom est obligatoire.")]
         [MinLength(2, ErrorMessage = "Le champs Prénom doit contenir au minimum 2 caractères.")]
         [MaxLength(64, ErrorMessage = "Le champs Prénom doit contenir au maximum 64 caractères.")]
         public string FirstName { get; set; }
 
+        [DisplayName("Date de naissance : ")]
         [Required(ErrorMessage = "Le champ Date de naissance est obligatoire.")]
         [DataType(DataType.Date)]
         public DateOnly BirthDate { get; set; }
