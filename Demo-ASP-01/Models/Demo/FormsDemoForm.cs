8d9fc18c638a6bb68a5df6c472a3880dc995704b
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-24 10:57:59 +0100

    Update Demo : ASP.net MVC - Formulaires (HtmlHelpers - Part02)

--- Demo-ASP-01/Models/Demo/FormsDemoForm.cs
+++ Demo-ASP-01/Models/Demo/FormsDemoForm.cs
@@ -4,6 +4,8 @@ namespace Demo_ASP_01.Models.Demo
 {
     public class FormsDemoForm
     {
+        [Required(ErrorMessage = "Veuillez choisir un Titre")]
+        public string Title { get; set; }
         [Required(ErrorMessage = "Le champs Nom de famille est obligatoire.")]
         [MinLength(2, ErrorMessage = "Le champs Nom de famille doit contenir au minimum 2 caractères.")]
         [MaxLength(64, ErrorMessage = "Le champs Nom de famille doit contenir au maximum 64 caractères.")]
@@ -15,6 +17,7 @@ namespace Demo_ASP_01.Models.Demo
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Le champ Date de naissance est obligatoire.")]
+        [DataType(DataType.Date)]
         public DateOnly BirthDate { get; set; }
     }
 }
